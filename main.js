df1b96da319751b33b310e450b953b8f5ea49459
KorotenkoS <sergey588@ua.fm>
2019-11-14 14:27:26 +0200

    zakonchil-rabotaet

--- main.js
+++ main.js
@@ -8,12 +8,40 @@ var btnInput = document.querySelector('#vvod-knopka');
 
 var btnDellet = document.querySelector('#vvod-delete');
 
+var posting = document.querySelector('#posts-list')
+
 btnInput.onclick = send;
 
-function send(arr) {
-    for (var data = {
-            dannie: inputWnd.value,
-        }
-        arr.push(data)
-    ) { console.log(arr); }
+btnDellet.onclick = delAll
+
+inputWnd.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+        // можете делать все что угодно со значением текстового поля
+        send();
+    }
+});
+
+function send() {
+    if (inputWnd.value) {
+        arr.push(inputWnd.value)
+        inputWnd.value = ""
+        console.log(arr);
+        render();
+    } else(
+        alert("Напиши чего-нибудь, а потом жми!")
+    )
+};
+
+function delAll() {
+    arr = [];
+    console.log(arr);
+    render();
+};
+
+function render() {
+    var html = '';
+    for (var value of arr) {
+        html += `<li>${value}</li>`
+    };
+    posting.innerHTML = html;
 };
